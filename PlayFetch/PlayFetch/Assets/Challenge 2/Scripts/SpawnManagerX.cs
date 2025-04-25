ace426e136442c37ceb7f21be84e0f543482c9b2
_[gen]_ <refflerg@roverkids.org>
2025-04-25 14:11:26 -0400

    Working

--- PlayFetch/PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ PlayFetch/PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -22,11 +22,13 @@ public class SpawnManagerX : MonoBehaviour
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
